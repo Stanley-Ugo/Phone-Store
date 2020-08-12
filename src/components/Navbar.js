3590226f6126b54d00c170e80ad08e34dfdd3ec9
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-13 00:54:24 +0100

    importing the Link component to the Navbar Component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h3>Hello from Navbar</h3>
-            </div>
+            <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+                <Link to="/">
+                    
+                </Link>
+            </nav>
         )
     }
 }
