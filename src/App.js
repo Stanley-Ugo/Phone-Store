32ae0506917c0e1cc9c92f6d4f956ca62438cbfa
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-13 01:20:51 +0100

    adding Links to /product and /cart in the Navbar component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import ProductList from './components/ProductList';
-import Details from './components/Default';
+import Details from './components/Details';
 import Cart from './components/Cart';
 import Default from './components/Default';
 
@@ -15,12 +15,10 @@ function App() {
       <Navbar/>
       <Switch>
         <Route exact path="/" component={ProductList}/>
-        <Route path="/details" component={Details}/>
-        <Route path="/cart" component={Cart}/>
+        <Route exact path="/details" component={Details}/>
+        <Route exact path="/cart" component={Cart}/>
         <Route component={Default}/>
       </Switch>
-      <Cart/>
-      <Default/>
     </React.Fragment>
   );
 }
