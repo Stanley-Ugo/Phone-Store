70e2c716bff1d0897794495dee6a0ea1bd521e4d
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-13 02:06:13 +0100

    importing and using the ButtonContainer variable from Button.js in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
 //import Styled from 'styled-components';
-
+import { ButtonContainer } from './Button';
 
 export default class Navbar extends Component {
     render() {
