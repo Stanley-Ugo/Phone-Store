2c09732774912bba826e035f422f83e185502a9d
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-14 23:17:48 +0100

    using conditional rendering to dispaly font-awesome's cart icon

--- src/components/Product.js
+++ src/components/Product.js
@@ -17,7 +17,7 @@ export default class Product extends Component {
                         <button className="cart-btn" disabled={inCart ? true : false }
                         onClick={()=> {console.log('added to the cart')}}
                         >
-                            
+                            {inCart ? (<p className="text-capitalize mb-0" disabled>{ "" }in cart</p>) : (<i className="fas fa-cart-plus"/>)}
                         </button>
                     </div>
                 </div>
