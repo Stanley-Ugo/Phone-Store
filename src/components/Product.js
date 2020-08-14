ed795f388772d699c1217da11f4f2b29706c7454
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-14 23:08:12 +0100

    adding a button in the Products.js file

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,10 +10,13 @@ export default class Product extends Component {
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
-                    <div className="img-container p-5" onClick={console.log('you clicked me')}>
+                    <div className="img-container p-5" onClick={() => console.log('you clicked me')}>
                         <Link to="/details">
                             <img src={img} alt="Product" className="car-img-top"/>
                         </Link>
+                        <button className="cart-btn" disabled={inCart ? true : false }>
+
+                        </button>
                     </div>
                 </div>
             </ProductWrapper>
