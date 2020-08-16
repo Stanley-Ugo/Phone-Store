80a1fabb7df57a0324e75597565616d74a701d28
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-16 08:43:29 +0100

    adding openModel() to the click event of Product.js & Detail.js

--- src/components/Details.js
+++ src/components/Details.js
@@ -62,7 +62,8 @@ export default class Details extends Component {
                       cart
                       disabled={inCart ? true : false}
                       onClick={()=> {
-                          value.addToCart(id)
+                          value.addToCart(id);
+                          value.openModel(id);
                       }}
                       >
                           {inCart ? 'In Cart' : 'Add To Cart'}
