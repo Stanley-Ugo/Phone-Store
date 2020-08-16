04a3da3ec20b0d56ee9fedda8ef70b23a3a82359
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-16 17:35:20 +0100

    adding then Price to the Img in Model.js

--- src/components/Model.js
+++ src/components/Model.js
@@ -18,13 +18,17 @@ export default class Model extends Component {
             return (
               <ModelContainer>
                 <div className="container">
-                    <div className="row">
-                        <div id="model" className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize p-5">
-                            <h5>Item added to the Cart</h5>
-                            <img src={img} className="img-fluid" alt="product" />
-                            <h5>{title}</h5>
-                        </div>
+                  <div className="row">
+                    <div
+                      id="model"
+                      className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize p-5"
+                    >
+                      <h5>Item added to the Cart</h5>
+                      <img src={img} className="img-fluid" alt="product" />
+                      <h5>{title}</h5>
+                      <h5 className="text-muted">price: $ {price}</h5>
                     </div>
+                  </div>
                 </div>
               </ModelContainer>
             );
@@ -36,16 +40,16 @@ export default class Model extends Component {
 }
 
 const ModelContainer = styled.div`
-position: fixed;
-top: 0;
-left: 0;
-right: 0;
-bottom: 0;
-background: rgba(0,0,0,0.3);
-display: flex;
-align-items: center;
-justify-content: center;
-#model {
-  background: var(--mainWhite);
-}
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.3);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  #model {
+    background: var(--mainWhite);
+  }
 `;
