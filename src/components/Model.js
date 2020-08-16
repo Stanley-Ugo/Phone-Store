aef58f506d2ce6071255a0368673762da5ea54b3
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-16 17:39:47 +0100

    adding a <Link/> and a <ButtonContainer/> to=/ Home to Continue shopping

--- src/components/Model.js
+++ src/components/Model.js
@@ -27,6 +27,11 @@ export default class Model extends Component {
                       <img src={img} className="img-fluid" alt="product" />
                       <h5>{title}</h5>
                       <h5 className="text-muted">price: $ {price}</h5>
+                      <Link to="/">
+                        <ButtonContainer onClick={}>
+                          Continue Shopping
+                        </ButtonContainer>
+                      </Link>
                     </div>
                   </div>
                 </div>
