c937f427802577d7110543d0f5500d65449898ea
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-16 17:28:17 +0100

    adding the Styled.Component to Model.js <ModelContainer/>

--- src/components/Model.js
+++ src/components/Model.js
@@ -34,4 +34,16 @@ export default class Model extends Component {
 }
 
 const ModelContainer = styled.div`
+position: fixed;
+top: 0;
+left: 0;
+right: 0;
+bottom: 0;
+background: rgba(0,0,0,0.3);
+display: flex;
+align-items: center;
+justify-content: center;
+#model {
+  background: var(--mainWhite);
+}
 `;
