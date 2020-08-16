660e750eba36cf7aab907abc60ebea08aff417fe
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-17 00:17:34 +0100

    adding a CartItem Component to Cart folder

--- src/components/Cart/CartList.js
+++ src/components/Cart/CartList.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import CartItem from './CartItem';
 
 export default function CartList() {
     return (
         <div>
             <h1>Hello from Cart List</h1>
+            <CartItem />
         </div>
     )
 }
