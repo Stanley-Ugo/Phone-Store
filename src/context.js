595f5e466f35a22b182ae89dc7e3da83ac5c862f
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-18 00:22:29 +0100

    adding increment(id); functionality in the context.js

--- src/context.js
+++ src/context.js
@@ -74,7 +74,20 @@ class ProductProvider extends Component {
     }
 
     increment = (id) => {
-        console.log('this is Increment method');
+        let tempCart = [...this.state.cart];
+        const selectProduct = tempCart.find(item => item.id === id);
+
+        const index = tempCart.indexOf(selectProduct);
+        const product = tempCart[index];
+
+        product.count = product.count + 1;
+        product.total = product.count * product.price;
+
+        this.setState(() => {
+            return {cart: [...tempCart]}
+        }, () => {
+            this.addTotals();
+        })
     }
 
     decrement = (id) => {
